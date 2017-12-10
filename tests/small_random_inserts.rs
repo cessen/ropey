4ec346e0dab6be35ae0f38dd6de235f6b16f718f
Nathan Vegdahl <cessen@cessen.com>
2017-12-10 10:31:18 -0800

    Added a tests directory and some larger tests.

---
+++ tests/small_random_inserts.rs
@@ -0,0 +1,32 @@
+extern crate ropey;
+extern crate rand;
+
+use ropey::Rope;
+use rand::Rng;
+
+#[test]
+fn small_random_inserts() {
+    let mut rng = rand::thread_rng();
+    let mut tree = Rope::new();
+
+    // Do a bunch of random incoherent inserts
+    for _ in 0..(1 << 12) {
+        let len = tree.len_chars().max(1);
+        tree.insert(rng.gen::<usize>() % len, "Hello ");
+        tree.insert(rng.gen::<usize>() % len, "world! ");
+        tree.insert(rng.gen::<usize>() % len, "How are ");
+        tree.insert(rng.gen::<usize>() % len, "you ");
+        tree.insert(rng.gen::<usize>() % len, "doing?\r\n");
+        tree.insert(rng.gen::<usize>() % len, "Let's ");
+        tree.insert(rng.gen::<usize>() % len, "keep ");
+        tree.insert(rng.gen::<usize>() % len, "inserting ");
+        tree.insert(rng.gen::<usize>() % len, "more ");
+        tree.insert(rng.gen::<usize>() % len, "items.\r\n");
+        tree.insert(rng.gen::<usize>() % len, "こんいちは、");
+        tree.insert(rng.gen::<usize>() % len, "みんなさん！");
+    }
+
+    // Make sure the tree is sound
+    tree.assert_integrity();
+    tree.assert_invariants();
+}
