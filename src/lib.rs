8ffc7ace8ff1e15d6aff1e4cd553316392a6f3c3
Nathan Vegdahl <cessen@cessen.com>
2017-12-18 20:19:14 -0800

    Shorter and sweeter documentation intro.
    Eventually we'll want a proper intro with an API summary and
    examples, but this should do for now.

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,15 @@
-//! A ut8 text rope for efficient editing of large texts.
+//! Ropey provides a unicode-aware implementation of utf8 text ropes for Rust.
+//! It is designed with the following in mind:
 //!
-//! Ropey provides a unicode-aware implementation of text ropes for Rust.
-//! It does double-duty as both the mutate-in-place and copy-on-write
-//! variants of text ropes (it just depends on how you use the APIs).
-//!
-//! Ropey is designed with the following in mind:
-//!
-//! - **Strong Unicode support.**  Ropey treats `char`'s (unicode code points)
-//!   as the base unit of text.  You can index into, slice by, and iterate over
-//!   a Ropey-rope by `char` index.
-//! - **Line-aware.**  Ropey maintains meta-data about line breaks, allowing
-//!   you to index into and iterate over lines of text.
-//! - **Grapheme-friendly.**  Ropey ensures that graphemes are never split in its
-//!   internal representation of text, and provides APIs for iterating over
-//!   graphemes and querying about grapheme boundaries.
-//! - **Frequent edits of large texts.**  Ropey is intended to be used for text
-//!   editing and manipulation, including when the text is hundreds of megabytes
-//!   large and the edits are all over the place.
-//! - **Thread safety.** Data is shared between clones of Ropey-ropes, making
-//!   clones extremley cheap. This is entirely thread safe, and clones can be
-//!   freely sent between threads.  More memory is only taken up incrementally
-//!   as edits cause clones to diverge.
-//! - **Efficiency.**  All of the above is fast and minimizes memory usage.
+//! - **Text editing.**  Ropey is specifically designed with text editing
+//!   in mind, including editing of very large texts (e.g. hundreds of
+//!   megabytes).
+//! - **Strong Unicode support.**  Ropey treats Unicode code points as the
+//!   base unit of text.  It also provides facilities for working with grapheme
+//!   clusters.
+//! - **Line-aware.**  Ropey knows about line breaks, so you can index into
+//!   and iterate over lines of text.
+//! - **Efficiency.**  Ropey aims to be fast and to minimize memory usage.
 
 
 extern crate smallvec;
