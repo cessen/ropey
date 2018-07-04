b343f50ac3be747d06b0668eefd356e95a956255
Nathan Vegdahl <cessen@cessen.com>
2018-07-04 11:40:51 -0700

    Added chunk-by-byte/char-index methods to RopeSlice as well.

--- src/slice.rs
+++ src/slice.rs
@@ -4,7 +4,7 @@ use std::sync::Arc;
 
 use iter::{Bytes, Chars, Chunks, Lines};
 use rope::Rope;
-use str_utils::char_idx_to_byte_idx;
+use str_utils::{byte_idx_to_char_idx, char_idx_to_byte_idx};
 use tree::{Count, Node};
 
 /// An immutable view into part of a `Rope`.
@@ -212,6 +212,73 @@ impl<'a> RopeSlice<'a> {
         let end = self.line_to_char(line_idx + 1);
 
         self.slice(start..end)
+    }
+
+    /// Returns the chunk containing the given byte index, along with
+    /// the byte offset of that byte within the chunk.
+    ///
+    /// # Panics
+    ///
+    /// Panics if `byte_idx` is out of bounds (i.e. `byte_idx >= len_bytes()`).
+    pub fn chunk_at_byte(&self, byte_idx: usize) -> (&str, usize) {
+        // Bounds check
+        assert!(
+            byte_idx < self.len_bytes(),
+            "Attempt to index past end of slice: byte index {}, slice byte length {}",
+            byte_idx,
+            self.len_bytes()
+        );
+
+        // Get the chunk.
+        let (chunk, byte_offset) = self.node
+            .get_chunk_at_byte(byte_idx + self.start_byte as usize);
+
+        // Calculate clipped start/end byte indices within the chunk.
+        let chunk_start_idx = if byte_offset > byte_idx {
+            byte_offset - byte_idx
+        } else {
+            0
+        };
+        let chunk_end_idx = {
+            let tmp = byte_idx + self.start_byte as usize + chunk.len() - byte_offset;
+            if tmp > self.end_byte as usize {
+                tmp - self.end_byte as usize
+            } else {
+                chunk.len()
+            }
+        };
+
+        // Return the clipped chunk and byte offset.
+        (
+            &chunk[chunk_start_idx..chunk_end_idx],
+            byte_offset - chunk_start_idx,
+        )
+    }
+
+    /// Returns the chunk containing the given char index, along with
+    /// the char and byte offset of that char within the chunk.
+    ///
+    /// The return is organized as (chunk, char_offset, byte_offset).
+    ///
+    /// # Panics
+    ///
+    /// Panics if `char_idx` is out of bounds (i.e. `char_idx >= len_chars()`).
+    pub fn chunk_at_char(&self, char_idx: usize) -> (&str, usize, usize) {
+        // Bounds check
+        assert!(
+            char_idx < self.len_chars(),
+            "Attempt to index past end of slice: char index {}, slice char length {}",
+            char_idx,
+            self.len_chars()
+        );
+
+        // TODO: get the chunk with byte and char index in one go,
+        // to avoid traversing the rope tree twice.
+        let byte_idx = self.char_to_byte(char_idx);
+        let (chunk, byte_offset) = self.chunk_at_byte(byte_idx);
+        let char_offset = byte_idx_to_char_idx(chunk, byte_offset);
+
+        (chunk, char_offset, byte_offset)
     }
 
     //-----------------------------------------------------------------------
