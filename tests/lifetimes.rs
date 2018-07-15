78b0caadddf962b484f9c82c3508972fe9c147ae
Nathan Vegdahl <cessen@cessen.com>
2018-07-14 20:12:56 -0700

    Made the lifetime test-cases slightly more robust.

--- tests/lifetimes.rs
+++ tests/lifetimes.rs
@@ -1,7 +1,6 @@
 //! This test file ensures that all of the lifetimes work the way we
 //! want, and that there are no regressions. It's a "does this compile?"
 //! test.
-#![allow(unused)]
 
 extern crate ropey;
 
@@ -12,38 +11,47 @@ const TEXT: &str = include_str!("test_text.txt");
 fn main() {
     let rope = Rope::from_str(TEXT);
 
-    // The lifetimes of the intermediate slices shouldn't matter,
-    // and all of this should compile.  The lifetimes of the things
-    // produced by these calls should be tied to the lifetime of the
-    // original rope, not the lifetime of the slice they were created
-    // from.
-    let a = rope.slice(4..500).slice(4..400).slice(4..300);
-    let b = rope.slice(4..500).slice(4..400).as_str();
-    let c = rope.slice(4..500).slice(4..400).line(1);
-    let d = rope.slice(4..500).slice(4..400).chunk_at_byte(50);
-    let e = rope.slice(4..500).slice(4..400).chunk_at_char(50);
-    let f = rope.slice(4..500).slice(4..400).chunk_at_line_break(3);
+    let (a, b, c, d, e, f, count, line, string) = {
+        // The lifetimes of the intermediate slices shouldn't matter,
+        // and all of this should compile.  The lifetimes of the things
+        // produced by these calls should be tied to the lifetime of the
+        // original rope, not the lifetime of the slice they were created
+        // from.
+        let a = rope.slice(4..500).slice(4..400).slice(4..300);
+        let b = rope.slice(4..500).slice(4..400).as_str();
+        let c = rope.slice(4..500).slice(4..400).line(1);
+        let d = rope.slice(4..500).slice(4..400).chunk_at_byte(50);
+        let e = rope.slice(4..500).slice(4..400).chunk_at_char(50);
+        let f = rope.slice(4..500).slice(4..400).chunk_at_line_break(3);
 
-    // Same for iterators.  In addition, the items _yielded_ by the
-    // iterators should also be tied to the lifetime of the original
-    // rope, not to the iterator or slice they came from.
+        // Same for iterators.  In addition, the items _yielded_ by the
+        // iterators should also be tied to the lifetime of the original
+        // rope, not to the iterator or slice they came from.
 
-    let mut count = 0;
-    for _ in rope.slice(4..500).slice(4..400).bytes() {
-        count += 1;
-    }
-    for _ in rope.slice(4..500).slice(4..400).chars() {
-        count += 1;
-    }
+        let mut count = 0;
+        for _ in rope.slice(4..500).slice(4..400).bytes() {
+            count += 1;
+        }
+        for _ in rope.slice(4..500).slice(4..400).chars() {
+            count += 1;
+        }
 
-    let mut line = RopeSlice::from_str("");
-    for l in rope.slice(4..500).slice(4..400).lines() {
-        line = l;
-    }
-    line = line.slice(..).slice(..);
+        let mut line = RopeSlice::from_str("");
+        for l in rope.slice(4..500).slice(4..400).lines() {
+            line = l;
+        }
+        line = line.slice(..).slice(..);
 
-    let mut string = "";
-    for c in rope.slice(4..500).slice(4..400).chunks() {
-        string = c;
-    }
+        let mut string = "";
+        for c in rope.slice(4..500).slice(4..400).chunks() {
+            string = c;
+        }
+
+        (a, b, c, d, e, f, count, line, string)
+    };
+
+    println!(
+        "{} {:?} {} {:?} {:?} {:?} {} {} {}",
+        a, b, c, d, e, f, count, line, string
+    );
 }
