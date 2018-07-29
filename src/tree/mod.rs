cadbc4301d75b1e1143845f9adc0ff71b2179d29
Nathan Vegdahl <cessen@cessen.com>
2018-07-29 14:21:10 -0700

    Tweaking tree parameters to better handle corner cases.

--- src/tree/mod.rs
+++ src/tree/mod.rs
@@ -25,17 +25,21 @@ const TARGET_NODE_SIZE: usize = 1024 - (PTR_SIZE * 2);
 // Node min/max values.
 // For testing, they're set small to trigger deeper trees.  For
 // non-testing, they're determined by TARGET_NODE_SIZE, above.
+// MAX_BYTES must be >= 4 to allow for 4-byte utf8 characters.
 #[cfg(test)]
 pub(crate) const MAX_CHILDREN: usize = 5;
 #[cfg(not(test))]
 pub(crate) const MAX_CHILDREN: usize = (TARGET_NODE_SIZE - 1) / CHILD_INFO_SIZE;
-pub(crate) const MIN_CHILDREN: usize = MAX_CHILDREN - (MAX_CHILDREN / 2);
+pub(crate) const MIN_CHILDREN: usize = MAX_CHILDREN / 2;
 
 #[cfg(test)]
 pub(crate) const MAX_BYTES: usize = 9; // Note: can't be 8, because 3-byte characters.
 #[cfg(not(test))]
 pub(crate) const MAX_BYTES: usize = TARGET_NODE_SIZE - 1 - (PTR_SIZE * 2);
-pub(crate) const MIN_BYTES: usize = MAX_BYTES - (MAX_BYTES / 2);
+// MIN_BYTES is little smaller than half MAX_BYTES so that repeated
+// splitting/merging doesn't happen on alternating small insertions and
+// removals.
+pub(crate) const MIN_BYTES: usize = (MAX_BYTES / 2) - (MAX_BYTES / 32);
 
 // Type used for storing tree metadata, such as byte and char length.
 pub(crate) type Count = u64;
