aa413868c7922833dfc5d7bb8e7d1130c150a543
Nathan Vegdahl <cessen@cessen.com>
2019-01-01 23:10:44 -0800

    Better comments and misc cleanup of search-and-replace example.

--- examples/search_and_replace.rs
+++ examples/search_and_replace.rs
@@ -156,34 +156,38 @@ impl<'a> Iterator for SearchIter<'a> {
     // Return the start/end char indices of the next match.
     fn next(&mut self) -> Option<(usize, usize)> {
         while let Some(next_char) = self.char_iter.next() {
-            // Push current position onto the possible matches list, to seed a
-            // possible beginning of a match.
+            self.cur_index += 1;
+
+            // Push new potential match, for a possible match starting at the
+            // current char.
             self.possible_matches.push(self.search_pattern.chars());
 
-            // Check it against the next character in each of the potential
-            // matches, removing the potential matches that don't match.
-            // We're using indexing instead of iteration here so that we can
-            // remove the possible matches as we go.
+            // Check the rope's char against the next character in each of
+            // the potential matches, removing the potential matches that
+            // don't match.  We're using indexing instead of iteration here
+            // so that we can remove the possible matches as we go.
             let mut i = 0;
             while i < self.possible_matches.len() {
                 let pattern_char = self.possible_matches[i].next().unwrap();
                 if next_char == pattern_char {
                     if self.possible_matches[i].clone().next() == None {
-                        self.cur_index += 1;
+                        // We have a match!  Reset possible matches and
+                        // return the successful match's char indices.
                         let char_match_range = (
                             self.cur_index - self.search_pattern_char_len,
                             self.cur_index,
                         );
                         self.possible_matches.clear();
                         return Some(char_match_range);
+                    } else {
+                        // Match isn't complete yet, move on to the next.
+                        i += 1;
                     }
-                    i += 1;
                 } else {
+                    // Doesn't match, remove it.
                     self.possible_matches.swap_remove(i);
                 }
             }
-
-            self.cur_index += 1;
         }
 
         return None;
