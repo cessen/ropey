d5ac182cf4d191eb1891502cb775c02a072e0152
Nathan Vegdahl <cessen@cessen.com>
2020-04-04 11:41:02 +0900

    Use different node sizes for internal and leaf nodes.

--- src/tree/mod.rs
+++ src/tree/mod.rs
@@ -4,7 +4,7 @@ mod node_text;
 mod text_info;
 
 #[cfg(not(test))]
-use std::{mem::size_of, sync::Arc};
+use std::mem::size_of;
 
 pub(crate) use self::node::Node;
 pub(crate) use self::node_children::NodeChildren;
@@ -14,28 +14,28 @@ pub(crate) use self::text_info::TextInfo;
 #[cfg(not(test))]
 const PTR_SIZE: usize = size_of::<&u8>();
 #[cfg(not(test))]
-const CHILD_INFO_SIZE: usize = size_of::<Arc<Node>>() + size_of::<TextInfo>();
+const CHILD_INFO_SIZE: usize = size_of::<Node>() + size_of::<TextInfo>();
 
-// Aim for nodes to be 1024 bytes minus Arc counters.  Keeping the nodes
-// multiples of large powers of two makes it easier for the memory allocator
-// to avoid fragmentation.
+// Aim for nodes to be a power-of-two bytes minus Arc counters.
 #[cfg(not(test))]
-const TARGET_NODE_SIZE: usize = 1024 - (PTR_SIZE * 2);
+const TARGET_LEAF_NODE_SIZE: usize = 1024 - (PTR_SIZE * 2);
+#[cfg(not(test))]
+const TARGET_INTERNAL_NODE_SIZE: usize = 512 - (PTR_SIZE * 2);
 
 // Node min/max values.
 // For testing, they're set small to trigger deeper trees.  For
-// non-testing, they're determined by TARGET_NODE_SIZE, above.
+// non-testing, they're determined by TARGET_*_NODE_SIZE, above.
 // MAX_BYTES must be >= 4 to allow for 4-byte utf8 characters.
 #[cfg(test)]
 pub(crate) const MAX_CHILDREN: usize = 5;
 #[cfg(not(test))]
-pub(crate) const MAX_CHILDREN: usize = (TARGET_NODE_SIZE - 1) / CHILD_INFO_SIZE;
+pub(crate) const MAX_CHILDREN: usize = (TARGET_INTERNAL_NODE_SIZE - 1) / CHILD_INFO_SIZE;
 pub(crate) const MIN_CHILDREN: usize = MAX_CHILDREN / 2;
 
 #[cfg(test)]
 pub(crate) const MAX_BYTES: usize = 9; // Note: can't be 8, because 3-byte characters.
 #[cfg(not(test))]
-pub(crate) const MAX_BYTES: usize = TARGET_NODE_SIZE - 1 - (PTR_SIZE * 2);
+pub(crate) const MAX_BYTES: usize = TARGET_LEAF_NODE_SIZE - 1 - (PTR_SIZE * 2);
 // MIN_BYTES is little smaller than half MAX_BYTES so that repeated
 // splitting/merging doesn't happen on alternating small insertions and
 // removals.
