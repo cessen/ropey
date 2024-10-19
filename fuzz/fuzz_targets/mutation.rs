d7321bccb8c7aeef4ca8ac96a75e6f2461a72874
Nathan Vegdahl <cessen@cessen.com>
2024-10-19 15:12:56 +0200

    Add fuzz testing.

---
+++ fuzz/fuzz_targets/mutation.rs
@@ -0,0 +1,48 @@
+#![no_main]
+
+use libfuzzer_sys::{
+    arbitrary::{self, Arbitrary},
+    fuzz_target,
+};
+use ropey::Rope;
+
+const SMALL_TEXT: &str = include_str!("small.txt");
+
+#[derive(Arbitrary, Copy, Clone, Debug)]
+enum Op<'a> {
+    Insert(usize, &'a str),
+    InsertChar(usize, char),
+    Remove(usize, usize),
+}
+
+#[derive(Arbitrary, Copy, Clone, Debug)]
+enum StartingText<'a> {
+    Small,
+    Medium,
+    Custom(&'a str),
+}
+
+fuzz_target!(|data: (StartingText, Vec<Op>)| {
+    let mut r = match data.0 {
+        StartingText::Small => Rope::from_str(SMALL_TEXT),
+        StartingText::Medium => Rope::from_str(&SMALL_TEXT.repeat(100)),
+        StartingText::Custom(s) => Rope::from_str(s),
+    };
+
+    for op in data.1 {
+        match op {
+            Op::Insert(idx, s) => {
+                let _ = r.try_insert(idx, s);
+            }
+            Op::InsertChar(idx, c) => {
+                let _ = r.try_insert_char(idx, c);
+            }
+            Op::Remove(idx_1, idx_2) => {
+                let _ = r.try_remove(idx_1..idx_2);
+            }
+        }
+    }
+
+    // r.assert_integrity();
+    r.assert_invariants();
+});
