5267f409f831e224743adf20c6dc87887f16d8fd
Nathan Vegdahl <cessen@cessen.com>
2024-10-20 09:32:16 +0200

    Add github CI and issue template.

---
+++ .github/ISSUE_TEMPLATE/bug_report.md
@@ -0,0 +1,14 @@
+---
+name: Bug report
+about: Report bugs and crashes
+title: ''
+labels: ''
+assignees: ''
+
+---
+
+**Ropey Version**
+The version of Ropey you encountered this bug in.  Please provide the full version number (e.g. "2.0.0-alpha-1", not "2").
+
+**Describe the bug**
+A description of the bug, including how to reproduce it (if possible).
--- scripts/test_external_with_small_chunks.sh
+++ scripts/test_external_with_small_chunks.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Run integration tests with small chunks.  Cargo doesn't compile the
-# main crate (Ropey) with "test" enabled for integration tests, so we
+# Run external tests with small chunks.  Cargo doesn't compile the
+# main crate (Ropey) with "test" enabled for external tests, so we
 # need to do that manually.
 cargo test --features "internal_dev_small_chunks, simd, metric_chars, metric_utf16, metric_lines_lf, metric_lines_lf_cr, metric_lines_unicode" --test '*' "$@"
