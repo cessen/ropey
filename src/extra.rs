b8757f737a016a00f470c58b03ba0935da26b2b1
Nathan Vegdahl <cessen@cessen.com>
2025-05-31 00:16:35 +0200

    Make `extra` functionality free functions.
    This is a bit more straightforward, and also makes the functionality
    clearly stand out from other Ropey functionality when used.

--- src/extra.rs
+++ src/extra.rs
@@ -13,76 +13,61 @@ use std::sync::Arc;
 
 use crate::{tree::Node, Rope, RopeSlice};
 
-pub trait RopeExt {
-    /// Returns true if this rope and `other` point to precisely the same
-    /// in-memory data.
-    ///
-    /// This happens when one of the ropes is a clone of the other and
-    /// neither have been modified since then.  Because clones initially
-    /// share all the same data, it can be useful to check if they still
-    /// point to precisely the same memory as a way of determining
-    /// whether they are both still unmodified.
-    ///
-    /// Note: this is distinct from checking for equality: two ropes can
-    /// have the same *contents* (equal) but be stored in different
-    /// memory locations (not instances).  Importantly, two clones that
-    /// post-cloning are modified identically will *not* be instances
-    /// anymore, even though they will have equal contents.
-    ///
-    /// Runs in O(1) time.
-    fn is_instance(&self, other: &Self) -> bool;
-}
-
-impl RopeExt for Rope {
-    #[inline]
-    fn is_instance(&self, other: &Self) -> bool {
-        match (&self.root, &other.root) {
-            (Node::Internal(a), Node::Internal(b)) => Arc::ptr_eq(a, b),
-            (Node::Leaf(a), Node::Leaf(b)) => Arc::ptr_eq(a, b),
-            _ => false,
-        }
+/// Returns true if both ropes point to precisely the same in-memory data.
+///
+/// This happens when one of the ropes is a clone of the other and
+/// neither have been modified since then.  Because clones initially
+/// share all the same data, it can be useful to check if they still
+/// point to precisely the same memory as a way of determining
+/// whether they are both still unmodified.
+///
+/// Note: this is distinct from checking for equality: two ropes can
+/// have the same *contents* (equal) but be stored in different
+/// memory locations (not instances).  Importantly, two clones that
+/// post-cloning are modified identically will *not* be instances
+/// anymore, even though they will have equal contents.
+///
+/// Runs in O(1) time.
+pub fn ropes_are_instances(a: &Rope, b: &Rope) -> bool {
+    match (&a.root, &b.root) {
+        (Node::Internal(a_root), Node::Internal(b_root)) => Arc::ptr_eq(a_root, b_root),
+        (Node::Leaf(a_root), Node::Leaf(b_root)) => Arc::ptr_eq(a_root, b_root),
+        _ => false,
     }
 }
 
-pub trait RopeSliceExt {
-    /// Creates a cheap, non-editable `Rope` from the `RopeSlice`.
-    ///
-    /// The resulting `Rope` is guaranteed to not take up any additional
-    /// space itself beyond a small constant size, instead referencing the
-    /// original data.  The difference between this and a `RopeSlice` is that
-    /// this co-owns the data with the original `Rope` just like a `Rope`
-    /// clone would, and thus can be passed around freely (e.g. across thread
-    /// boundaries).  Additionally, its existence doesn't prevent the original
-    /// `Rope` from being edited, dropped, etc.
-    ///
-    /// This is distinct from using `Into<Rope>` on a `RopeSlice`, which edits
-    /// the resulting `Rope`'s data to trim it to the range of the slice, which
-    /// is both more expensive and results in space overhead compared to this
-    /// method.  However, a `Rope` from `Into<Rope>` will be a normal editable
-    /// `Rope`, whereas `Rope`s produced from this method are read-only.
-    ///
-    /// **You probably don't need to use this method.**  Legitimate use cases
-    /// for it are rare, and you should stick to normal `Rope`s and `RopeSlice`s
-    /// when you can.
-    ///
-    /// Runs in O(1) time.  Space usage is constant unless the original `Rope`
-    /// is edited, causing the otherwise shared contents to diverge.
-    ///
-    /// # Panics
-    ///
-    /// This method does not panic itself.  However, if edits are attempted
-    /// on the resulting `Rope` with the panicking variants `insert()` and
-    /// `remove()`, they will panic.
-    fn to_owning_slice(&self) -> crate::Rope;
-}
-
-impl RopeSliceExt for RopeSlice<'_> {
-    #[inline]
-    fn to_owning_slice(&self) -> Rope {
-        Rope {
-            root: self.root.clone(),
-            root_info: *self.root_info,
-            owned_slice_byte_range: self.byte_range,
-        }
+/// Creates a cheap, non-editable `Rope` from a `RopeSlice`.
+///
+/// The resulting `Rope` is guaranteed to not take up any additional
+/// space itself beyond a small constant size, instead referencing the
+/// original data.  The difference between this and a `RopeSlice` is that
+/// this co-owns the data with the original `Rope` just like a `Rope`
+/// clone would, and thus can be passed around freely (e.g. across thread
+/// boundaries).  Additionally, its existence doesn't prevent the original
+/// `Rope` from being edited, dropped, etc.
+///
+/// This is distinct from using `Into<Rope>` on a `RopeSlice`, which edits
+/// the resulting `Rope`'s data to trim it to the range of the slice, which
+/// is both more expensive and results in space overhead compared to this
+/// function.  However, a `Rope` from `Into<Rope>` will be a normal editable
+/// `Rope`, whereas `Rope`s produced from this function are read-only.
+///
+/// **You probably don't need to use this function.**  Legitimate use cases
+/// for it are rare, and you should stick to normal `Rope`s and `RopeSlice`s
+/// when you can.
+///
+/// Runs in O(1) time.  Space usage is constant unless the original `Rope`
+/// is edited, causing the otherwise shared contents to diverge.
+///
+/// # Panics
+///
+/// This function does not panic itself.  However, if edits are attempted
+/// on the resulting `Rope` with the panicking variants `insert()` and
+/// `remove()`, they will panic.
+pub fn slice_to_owning_slice(slice: RopeSlice) -> Rope {
+    Rope {
+        root: slice.root.clone(),
+        root_info: *slice.root_info,
+        owned_slice_byte_range: slice.byte_range,
     }
 }
