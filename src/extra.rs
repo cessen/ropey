c936bde4e289bdde5c9dc35cc50d4dcaacaf058a
Nathan Vegdahl <cessen@cessen.com>
2025-06-21 12:48:03 +0200

    Re-wrap comments.

--- src/extra.rs
+++ src/extra.rs
@@ -3,35 +3,36 @@
 pub mod esoterica {
     //! Esoteric functionality.
     //!
-    //! **Warning:** the functions in this module expose you to esoterica of Ropey's
-    //! internal data model, and take you off the beaten path of Ropey's intended
-    //! API semantics.  They are nevertheless API promises and can be depended on to
-    //! continue functioning as documented.  However, you should **read their
-    //! documentation carefully** and make sure you fully understand exactly what
-    //! they do/don't promise before using them.
+    //! **Warning:** the functions in this module expose you to esoterica of
+    //! Ropey's internal data model, and take you off the beaten path of Ropey's
+    //! intended API semantics.  They are nevertheless API promises and can be
+    //! depended on to continue functioning as documented.  However, you should
+    //! **read their documentation carefully** and make sure you fully understand
+    //! exactly what they do/don't promise before using them.
 
     use std::sync::Arc;
 
     use crate::{slice::SliceInner, tree::Node, Rope, RopeSlice};
 
-    /// Returns true if both ropes internally point to the same memory and share the
-    /// same content.
+    /// Returns true if both ropes internally point to the same memory and share
+    /// the same content.
     ///
     /// This function's API promises are very specific.  The following two things
     /// and *only* the following two things are guaranteed:
     ///
-    /// 1. If rope A and rope B are *unmodified* clones of each other (i.e. no edits
-    ///    have been made to either since cloning), then this function returns true.
-    ///    This applies to both direct and indirect clones, since cloning is
-    ///    transitive.
+    /// 1. If rope `a` and rope `b` are *unmodified* clones of each other (i.e.
+    ///    no edits have been made to either since cloning), then this function
+    ///    returns true. This applies to both direct and indirect clones, since
+    ///    cloning is transitive.
     /// 2. If the text contents of rope A and rope B are different, then this
     ///    function returns false.
     ///
-    /// In all other cases, this function's return value is unspecified (may change
-    /// between non-breaking releases) and should not be relied on for program
-    /// correctness. An example of such a case: rope B is cloned from rope A, and
-    /// then the same edit is made to both ropes.  They have then both been modified
-    /// since cloning (not case 1), but they also compare equal (not case 2 either).
+    /// In all other cases, this function's return value is unspecified (may
+    /// change between non-breaking releases) and should not be relied on for
+    /// program correctness. An example of such a case: rope B is cloned from
+    /// rope A, and then the same edit is made to both ropes.  They have then
+    /// both been modified since cloning (not case 1), but they also compare
+    /// equal (not case 2 either).
     ///
     /// Runs in O(1) time.
     pub fn ropes_are_instances(a: &Rope, b: &Rope) -> bool {
@@ -51,14 +52,14 @@ pub mod esoterica {
     ///
     /// This function is like `Into<Rope>` (the normal way to make `Rope`s from
     /// `RopeSlice`s), but with the time/space complexity of `Rope` cloning.  In
-    /// exchange for this efficiency, there is the possibility of failure under some
-    /// circumstances.  On failure, returns `None`.
+    /// exchange for this efficiency, there is the possibility of failure under
+    /// some circumstances.  On failure, returns `None`.
     ///
-    /// Success is guaranteed for `RopeSlice`s created from `Rope`s or from
-    /// `Rope`-derived `RopeSlice`s.  Whether this function succeeds on `RopeSlice`s
-    /// constructed in other ways (namely `From<&str>`) is unspecified (may change
-    /// between non-breaking releases) and should not be relied on for program
-    /// correctness.
+    /// Success is guaranteed for `RopeSlice`s created from `Rope`s or
+    /// from `Rope`-derived `RopeSlice`s.  Whether this function succeeds
+    /// on `RopeSlice`s constructed in other ways (namely `From<&str>`) is
+    /// unspecified (may change between non-breaking releases) and should not be
+    /// relied on for program correctness.
     ///
     /// Like `Rope` cloning, runs in O(1) time, and the resulting `Rope` shares
     /// its data with the originating `Rope`, taking up O(1) additional space
