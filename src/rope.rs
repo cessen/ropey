f0cdf8c3b56cffd133f6d99ea67d96be56936f59
Billy Levin <billy.levin97@gmail.com>
2026-04-23 20:41:12 +0100

    add `get_floor_char_boundary` to `RopeNoPanic`

--- src/rope.rs
+++ src/rope.rs
@@ -824,6 +824,10 @@ impl<'current> RopeNoPanic<'current, 'current> for Rope {
     fn get_is_char_boundary(&self, byte_idx: usize) -> Option<bool> {
         self.get_is_char_boundary_impl(byte_idx)
     }
+
+    fn get_floor_char_boundary(&self, byte_idx: usize) -> Option<usize> {
+        self.get_floor_char_boundary_impl(byte_idx)
+    }
 }
 
 //==============================================================
